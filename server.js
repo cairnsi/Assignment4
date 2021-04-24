837f4ac1f4deb84bce67172edf64fb968e97478a
cairnsi <59540148+cairnsi@users.noreply.github.com>
2021-04-24 08:27:25 -0700

    second commit

--- server.js
+++ server.js
@@ -60,14 +60,14 @@ async function delete_Boat(id){
     const key = datastore.key([BOAT, parseInt(id,10)]);
 	var [boat] = await datastore.get(key);
 	//empty loads
-	/*
+	
 	boat.loads.forEach(async function(loadobj) {
 		const loadKey = datastore.key([LOAD, parseInt(loadobj.id,10)]);
 		var [load] = await datastore.get(loadKey);
 		load.carrier = null;
 		await datastore.update({"key":loadKey, "data":load});
 	})
-	*/
+	
 	//done empty
     return datastore.delete(key);
 }
@@ -91,7 +91,20 @@ function boat_loadSelf(item){
 	 return item;
 }
 
-
+async function boat_removeLoad(id,boatKey){
+	var [boat] = await datastore.get(boatKey);
+	var index =-1;
+	var i;
+	for (i = 0; i < boat.loads.length; i++) {
+		if(boat.loads[i].id == id){
+			index = i;
+		}
+	}
+	if(index>-1){
+		boat.loads.splice(index,1);
+	}
+	datastore.save({"key":boatKey, "data":boat});
+}
 
 /* ------------- End Boat Model Functions ------------- */
 
@@ -149,7 +162,7 @@ function get_Loads(req){
 async function delete_Load(id){
     const key = datastore.key([LOAD, parseInt(id,10)]);
 	//remove load from boat
-	/*
+	
 	var [load] = await datastore.get(key);
 	if(load.carrier != null){
 		const boatKey = datastore.key([BOAT, parseInt(load.carrier.id,10)]);
@@ -157,7 +170,7 @@ async function delete_Load(id){
 		var index =-1;
 		var i;
 		for (i = 0; i < boat.loads.length; i++) {
-			if(boats.loads[i].id == id){
+			if(boat.loads[i].id == id){
 				index = i;
 			}
 		}
@@ -167,7 +180,7 @@ async function delete_Load(id){
 		datastore.save({"key":boatKey, "data":boat});
 		
 	}
-	*/
+	
 	// done remove
     return datastore.delete(key);
 }
@@ -176,6 +189,7 @@ async function delete_Load(id){
 /* ------------- End load Model Functions ------------- */
 /* ------------- Boat Routes -------------------------- */
 app.get('/boats', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	var boats = get_Boats(req)
 	.then( (boats) => {
         res.status(200).json(boats);
@@ -185,6 +199,7 @@ app.get('/boats', async (req, res) => {
 
 
 app.post('/boats', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	if(!req.body.name || !req.body.type || !req.body.length){
 		error = {"Error": "The request object is missing at least one of the required attributes"}
 		res.status(400).send(error);
@@ -198,6 +213,7 @@ app.post('/boats', async (req, res) => {
 });
 
 app.delete('/boats/:id', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	const key = datastore.key([BOAT, parseInt(req.params.id,10)]);
 	boat = await get_Boat(key);
 	if(boat == null){
@@ -211,6 +227,7 @@ app.delete('/boats/:id', async (req, res) => {
 });
 
 app.patch('/boats/:id', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	if(!req.body.name || !req.body.type || !req.body.length){
 		error = {"Error": "The request object is missing at least one of the required attributes"}
 		res.status(400).send(error);
@@ -231,6 +248,7 @@ app.patch('/boats/:id', async (req, res) => {
 });
 
 app.get('/boats/:id', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	const key = datastore.key([BOAT, parseInt(req.params.id,10)]);
 	boat = await get_Boat(key);
 	if(boat == null){
@@ -244,9 +262,10 @@ app.get('/boats/:id', async (req, res) => {
 });
 
 
-/* ------------- Slip Routes -------------------------- */
+/* ------------- Load Routes -------------------------- */
 
 app.post('/loads', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	if(!req.body.volume || !req.body.content){
 		error = {"Error": "The request object is missing the required volume or content"}
 		res.status(400).send(error);
@@ -260,6 +279,7 @@ app.post('/loads', async (req, res) => {
 });
 
 app.get('/loads/:id', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	const key = datastore.key([LOAD, parseInt(req.params.id,10)]);
 	load = await get_Load(key);
 	if(load == null){
@@ -273,6 +293,7 @@ app.get('/loads/:id', async (req, res) => {
 });
 
 app.get('/loads', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	var loads = get_Loads(req)
 	.then( (loads) => {
         res.status(200).json(loads);
@@ -280,6 +301,7 @@ app.get('/loads', async (req, res) => {
 });
 
 app.delete('/loads/:id', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	const key = datastore.key([LOAD, parseInt(req.params.id,10)]);
 	load = await get_Load(key);
 	if(load == null){
@@ -292,7 +314,23 @@ app.delete('/loads/:id', async (req, res) => {
 	}
 });
 
+app.get('/boats/:boat_id/loads', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
+	const boat_key = datastore.key([BOAT, parseInt(req.params.boat_id,10)]);
+	[boat] = await datastore.get(boat_key);
+	if(boat==null){
+		error = {"Error": "The specified boat does not exist" }
+		res.status(404).send(error);
+		return;
+	}
+	else{
+		formattedBoat = await get_Boat(boat_key);
+		res.status(200).json(formattedBoat.loads);
+	}
+});
+
 app.put('/boats/:boat_id/loads/:load_id', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	const boat_key = datastore.key([BOAT, parseInt(req.params.boat_id,10)]);
 	[boat] = await datastore.get(boat_key);
 	const load_key = datastore.key([LOAD, parseInt(req.params.load_id,10)]);
@@ -317,28 +355,36 @@ app.put('/boats/:boat_id/loads/:load_id', async (req, res) => {
 	}
 });
 
-app.delete('/slips/:slip_id/:boat_id', async (req, res) => {
-	const slip_key = datastore.key([SLIP, parseInt(req.params.slip_id,10)]);
-	slip = await get_Slip(slip_key);
+
+app.delete('/boats/:boat_id/loads/:load_id', async (req, res) => {
+	address = req.protocol + "://" + req.get("host");
 	const boat_key = datastore.key([BOAT, parseInt(req.params.boat_id,10)]);
-	boat = await get_Boat(boat_key);
-	if(slip == null || boat == null){
-		error = {"Error": "No boat with this boat_id is at the slip with this slip_id" }
+	[boat] = await datastore.get(boat_key);
+	const load_key = datastore.key([LOAD, parseInt(req.params.load_id,10)]);
+	var [load] = await datastore.get(load_key);
+	if(load == null || boat==null){
+		error = {"Error": "The specified boat and/or load does not exist" }
 		res.status(404).send(error);
 		return;
 	}
+	load.id = load_key.id;
+	if(load.carrier == null){
+		error = {"Error": "The specified load is not on this boat" }
+		res.status(403).send(error);
+		return;
+	}
+	else if(load.carrier.id != boat_key.id){
+		error = {"Error": "The specified load is not on this boat" }
+		res.status(403).send(error);
+		return;
+	}
 	else{
-		var response = await slip_RemoveBoat(req.params.slip_id,req.params.boat_id);
-		if(response){
-			res.status(204).end()
-		}else{
-			error = {"Error": "No boat with this boat_id is at the slip with this slip_id" }
-			res.status(404).send(error);
-			return;
-		}
+		await boat_removeLoad(load.id,boat_key);
+		load.carrier = null;
+		await datastore.update({"key":load_key, "data":load});
+		res.status(204).end();
 	}
 });
-
 
 
 // Listen to the App Engine-specified port, or 8080 otherwise
